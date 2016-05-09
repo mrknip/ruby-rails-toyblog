5398151690ee06c44ec123faa098b3de6278fa1d
Drew Johnson <drew.johnson.dev@gmail.com>
2016-05-09 15:38:52 +0100

    Add login/out function

---
+++ app/controllers/author_sessions_controller.rb
@@ -0,0 +1,21 @@
+class AuthorSessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    puts "Email: #{params[:email]}"
+    if login(params[:email], params[:password])
+      puts "Got lgoin"
+      redirect_back_or_to(articles_path, notice: 'Logged in successfully')
+    else
+      flash.notice = 'Login failed'
+      puts "No params"
+      render action: :new
+    end
+  end
+
+  def destroy
+    logout
+    redirect_to(articles_path, notice: 'Logged out')
+  end
+end
